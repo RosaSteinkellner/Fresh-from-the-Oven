2eeace6a8c444b56357d73c08417b5b5a2d1ea0e
Rosa Steinkellner <rosasteinkellner@gmail.com>
2019-04-13 17:46:05 +0200

    Inserted first recipe template

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var recipe = require('./recipe.js')
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {});
+  res.render('index', recipe);
 });
 
 app.listen(3000, function () {
---
+++ recipe.js
@@ -0,0 +1,14 @@
+var ingredients = [
+    { name: "White flour", quantity: "1000 g" },
+    { name: "Water", quantity: "720g, 32°C to 35°C" },
+    { name: "Fine sea salt", quantity: "21 g" },
+    { name: "Instant dried yeast", quantity: "4 g" },
+]
+
+var recipe = {
+    title: "The Saturday White Bread",
+    description: "tbd",
+    ingredients,
+}
+
+module.exports = recipe
